e7ac71f391b933c30013afc978d60bd2c17614e5
Carolina Blank <carolina.f.blank@gmail.com>
2023-08-25 15:30:17 -0300

    Pesquisa artigo

--- Clustering.jl
+++ Clustering.jl
@@ -10,84 +10,234 @@ PLD[2,:,:] = PLD_excel[169:336,1:Ω]
 PLD[3,:,:] = PLD_excel[337:504,1:Ω]
 PLD[4,:,:] = PLD_excel[505:672,1:Ω]
 
-Y = mean(PLD[1,:,:],dims=1)
-
 Random.seed!(13124152)
 
-# cluster X into 2 clusters using K-means
-R = kmeans(Y, 3)
-
-@assert nclusters(R) == 3 # verify the number of clusters
-
-a = assignments(R) # get the assignments of points to clusters
-c = counts(R) # get the cluster sizes
-M = R.centers # get the cluster centers
-
-
-# make a random dataset with 1000 random 5-dimensional points
+# ==================== 1 ==================
 X = PLD[1,:,:]
 
 Random.seed!(13124152)
 
 # cluster X into 2 clusters using K-means
-R = kmeans(X, 3)
+R = kmeans(X, 2)
 
-@assert nclusters(R) == 3 # verify the number of clusters
+@assert nclusters(R) == 2 # verify the number of clusters
 
-a = assignments(R) # get the assignments of points to clusters
+a1 = assignments(R) # get the assignments of points to clusters
 c = counts(R) # get the cluster sizes
 M = R.centers # get the cluster centers
 
 counter1=1
 counter2=1
 
-for i in 1:Ω
-    if a[i] == 2
-        println(i)
-    end
-end
-
-
 X3 = zeros(4,168,c[1])
 X2 = zeros(4,168,c[2])
+v2 = zeros(74)
+v3 = zeros(126)
 
 for i in 1:Ω
-    if a[i] == 2
+    if a1[i] == 2
         for t in 1:T
             X2[t,:,counter1] = PLD[t,:,i]
         end
+        v2[counter1] = i
         counter1+=1
     else
         for t in 1:T
             X3[t,:,counter2] = PLD[t,:,i]
         end
+        v3[counter2] = i
         counter2+=1
     end
 end
 
+# ==================== 2 ==================
 R2 = kmeans(X2[2,:,:], 2)
 
 @assert nclusters(R2) == 2 # verify the number of clusters
 
-a = assignments(R2) # get the assignments of points to clusters
+a2 = assignments(R2) # get the assignments of points to clusters
 c = counts(R2) # get the cluster sizes
 M = R2.centers # get the cluster centers
 
 counter1=1
 counter2=1
-X4 = zeros(4,168,c[1])
-X5 = zeros(4,168,c[2])
+X4 = zeros(4,168,9)
+X5 = zeros(4,168,65)
+v4 = zeros(9)
+v5 = zeros(65)
 
-for i in 1:Ω
-    if a[i] == 1
+for i in 1:74
+    if a2[i] == 2
         for t in 1:T
-            X4[t,:,counter1] = PLD[t,:,i]
+            X4[t,:,counter1] = X2[t,:,i]
         end
+        v4[counter1] = v2[i]
         counter1+=1
     else
         for t in 1:T
-            X5[t,:,counter2] = PLD[t,:,i]
+            X5[t,:,counter2] = X2[t,:,i]
         end
+        v5[counter2] = v2[i]
+        counter2+=1
+    end
+end
+
+# ==================== 3 ==================
+R3 = kmeans(X3[2,:,:], 2)
+
+@assert nclusters(R3) == 2 # verify the number of clusters
+
+a3 = assignments(R3) # get the assignments of points to clusters
+c = counts(R3) # get the cluster sizes
+M = R3.centers # get the cluster centers
+
+counter1=1
+counter2=1
+X6 = zeros(4,168,55)
+X7 = zeros(4,168,71)
+v6 = zeros(55)
+v7 = zeros(71)
+
+for i in 1:126
+    if a3[i] == 2
+        for t in 1:T
+            X6[t,:,counter1] = X3[t,:,i]
+        end
+        v6[counter1] = v3[i]
+        counter1+=1
+    else
+        for t in 1:T
+            X7[t,:,counter2] = X3[t,:,i]
+        end
+        v7[counter2] = v3[i]
+        counter2+=1
+    end
+end
+
+# ==================== 4 ==================
+R4 = kmeans(X4[3,:,:], 2)
+
+@assert nclusters(R4) == 2 # verify the number of clusters
+
+a4 = assignments(R4) # get the assignments of points to clusters
+c = counts(R4) # get the cluster sizes
+M = R4.centers # get the cluster centers
+
+counter1=1
+counter2=1
+X8 = zeros(4,168,7)
+X9 = zeros(4,168,2)
+v8 = zeros(7)
+v9 = zeros(2)
+
+for i in 1:9
+    if a4[i] == 2
+        for t in 1:T
+            X8[t,:,counter1] = X4[t,:,i]
+        end
+        v8[counter1] = v4[i]
+        counter1+=1
+    else
+        for t in 1:T
+            X9[t,:,counter2] = X4[t,:,i]
+        end
+        v9[counter2] = v4[i]
+        counter2+=1
+    end
+end
+
+# ==================== 5 ==================
+R5 = kmeans(X5[3,:,:], 2)
+
+@assert nclusters(R5) == 2 # verify the number of clusters
+
+a5 = assignments(R5) # get the assignments of points to clusters
+c = counts(R5) # get the cluster sizes
+M = R5.centers # get the cluster centers
+
+counter1=1
+counter2=1
+X10 = zeros(4,168,18)
+X11 = zeros(4,168,47)
+v10 = zeros(18)
+v11 = zeros(47)
+
+for i in 1:65
+    if a5[i] == 1
+        for t in 1:T
+            X10[t,:,counter1] = X5[t,:,i]
+        end
+        v10[counter1] = v5[i]
+        counter1+=1
+    else
+        for t in 1:T
+            X11[t,:,counter2] = X5[t,:,i]
+        end
+        v11[counter2] = v5[i]
+        counter2+=1
+    end
+end
+
+# ==================== 6 ==================
+R6 = kmeans(X6[3,:,:], 2)
+
+@assert nclusters(R6) == 2 # verify the number of clusters
+
+a6 = assignments(R6) # get the assignments of points to clusters
+c = counts(R6) # get the cluster sizes
+M = R6.centers # get the cluster centers
+
+counter1=1
+counter2=1
+X12 = zeros(4,168,33)
+X13 = zeros(4,168,22)
+v12 = zeros(33)
+v13 = zeros(22)
+
+for i in 1:55
+    if a6[i] == 1
+        for t in 1:T
+            X12[t,:,counter1] = X6[t,:,i]
+        end
+        v12[counter1] = v6[i]
+        counter1+=1
+    else
+        for t in 1:T
+            X13[t,:,counter2] = X6[t,:,i]
+        end
+        v13[counter2] = v6[i]
+        counter2+=1
+    end
+end
+
+# ==================== 7 ==================
+R7 = kmeans(X7[3,:,:], 2)
+
+@assert nclusters(R7) == 2 # verify the number of clusters
+
+a7 = assignments(R7) # get the assignments of points to clusters
+c = counts(R7) # get the cluster sizes
+M = R7.centers # get the cluster centers
+
+counter1=1
+counter2=1
+X14 = zeros(4,168,19)
+X15 = zeros(4,168,52)
+v14 = zeros(19)
+v15 = zeros(52)
+
+for i in 1:71
+    if a7[i] == 2
+        for t in 1:T
+            X14[t,:,counter1] = X7[t,:,i]
+        end
+        v14[counter1] = v7[i]
+        counter1+=1
+    else
+        for t in 1:T
+            X15[t,:,counter2] = X7[t,:,i]
+        end
+        v15[counter2] = v7[i]
         counter2+=1
     end
 end
--- PrescriptiveProblem (with Clustering).jl
+++ PrescriptiveProblem (with Clustering).jl
@@ -4,7 +4,46 @@ using DataFrames
 using XLSX
 using Statistics
 
-
+function n(t,w)
+    if t == 1
+        n = 1
+    elseif t == 2
+        if w in v2
+            n = 2
+        else w in v3
+            n = 3
+        end
+    elseif t == 3
+        if w in v4
+            n = 4
+        elseif w in v5
+            n = 5
+        elseif w in v6
+            n = 6
+        elseif w in v7
+            n = 7
+        end
+    else
+        if w in v8
+            n = 8
+        elseif w in v9
+            n = 9
+        elseif w in v10
+            n = 10
+        elseif w in v11
+            n = 11
+        elseif w in v12
+            n = 12
+        elseif w in v13
+            n = 13
+        elseif w in v14
+            n = 14
+        elseif w in v15
+            n = 15
+        end
+    end
+return n
+end
 
 
 # Parametros do caso
@@ -31,36 +70,32 @@ PLD[4,:,:] = PLD_excel[505:672,1:Ω]
 
 
 # Parametros de contrato/quantidade
-xmin = ones(T)*(-1)
-xmax = ones(T)
+xmin = ones(15)*(-1)
+xmax = ones(15)
 ymin = 0
 ymax = 1
 q = 100         # Quantidade do contrato semenal
-λ = [
-    [mean(PLD[1,:,:])],
+media_PLD = [
+    mean(PLD[1,:,:]),
 
-    [mean(X2[2,:,:]), mean(X3[2,:,:])],
+    mean(X2[2,:,:]), mean(X3[2,:,:]),
 
-    [mean(PLD[3,:,1:23]), mean(PLD[3,:,24:45]), mean(PLD[3,:,46:67]),
-     mean(PLD[3,:,68:90]), mean(PLD[3,:,91:112]), mean(PLD[3,:,113:134]),
-     mean(PLD[3,:,135:156]), mean(PLD[3,:,157:178]), mean(PLD[3,:,179:200])],
+    mean(X4[3,:,:]), mean(X5[3,:,:]), 
+    mean(X6[3,:,:]), mean(X7[3,:,:]),
 
-    [mean(PLD[4,:,1:7]), mean(PLD[4,:,8:15]), mean(PLD[4,:,16:23]),
-     mean(PLD[4,:,24:30]), mean(PLD[4,:,31:37]), mean(PLD[4,:,38:45]),
-     mean(PLD[4,:,46:52]), mean(PLD[4,:,53:59]), mean(PLD[4,:,60:67]),
-     mean(PLD[4,:,68:74]), mean(PLD[4,:,75:82]), mean(PLD[4,:,83:90]),
-     mean(PLD[4,:,91:97]), mean(PLD[4,:,98:104]), mean(PLD[4,:,105:112]),
-     mean(PLD[4,:,113:119]), mean(PLD[4,:,120:126]), mean(PLD[4,:,127:134]),
-     mean(PLD[4,:,135:141]), mean(PLD[4,:,142:148]), mean(PLD[4,:,149:156]),
-     mean(PLD[4,:,157:163]), mean(PLD[4,:,164:170]), mean(PLD[4,:,171:178]),
-     mean(PLD[4,:,179:185]), mean(PLD[4,:,186:192]), mean(PLD[4,:,193:200])]
-]               # Preço contrato semanal
-Q = 100         # Quantidade do contral mensal
-P = 100         # Preço contrato mensal
+    mean(X8[4,:,:]), mean(X9[4,:,:]), 
+    mean(X10[4,:,:]), mean(X11[4,:,:]), 
+    mean(X12[4,:,:]), mean(X13[4,:,:]),
+    mean(X14[4,:,:]), mean(X15[4,:,:])
+]
+λ = 0.93.*media_PLD .+15.31     # Preço contrato semanal
+Q = 100                         # Quantidade do contrato mensal
+P = 0.93*mean(PLD) +20          # Preço contrato mensal
 
 # Parametros de risco
 p = 1 / Ω .* ones(Ω);
 α = 0.95
+ξ = 0.5
 
 # Otimização
 
@@ -69,25 +104,28 @@ model = Model(Gurobi.Optimizer)
 @variable(model, R[1:T,1:Ω])
 @variable(model, z)
 @variable(model, δ[1:Ω] >= 0)
-@variable(model, x[1:T])
+@variable(model, x[1:15])
 @variable(model, y)
 
 @objective(
     model,
     Max,
-    sum(p[w] *sum(R[t,w] for t in 1:T) for w in 1:Ω)
+    ξ * (z - sum(δ[w] * p[w] / (1 - α) for w in 1:Ω)) +
+    (1 - ξ) * sum(p[w] *sum(R[t,w] for t in 1:T) for w in 1:Ω)
 );
 
 @constraint(
     model,
     [t ∈ 1:T, w ∈ 1:Ω],
     R[t,w] ==
-    sum((g[t,h,w]*GF*PLD[t,h,w]) + (P - PLD[t,h,w])*Q*y + (λ[t][n(t,w)] - PLD[t,h,w])*q*x[t] for h in 1:H)
+    sum((g[t,h,w]*GF*PLD[t,h,w]) + (P - PLD[t,h,w])*Q*y + (λ[n(t,w)] - PLD[t,h,w])*q*x[n(t,w)] for h in 1:H)
 );
 
-@constraint(model, [t ∈ 1:T], xmin[t] <= x[t])
+@constraint(model, [t ∈ 1:T, w ∈ 1:Ω], Q*y + q*x[n(t,w)] <= GF)
 
-@constraint(model, [t ∈ 1:T], x[t] <= xmax[t])
+@constraint(model, [n ∈ 1:15], xmin[n] <= x[n])
+
+@constraint(model, [n ∈ 1:15], x[n] <= xmax[n])
 
 @constraint(model, ymin <= y)
 
